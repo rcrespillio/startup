b0915f780d95881e7ea3ccd5c274e5908749673b
rcrespillio <rc@live.com.ar>
2015-11-18 21:37:31 -0300

    Topic 2: Design Patterns and OOP in JavaScript

---
+++ 02-oop-inheritance/myScript.js
@@ -0,0 +1,78 @@
+
+            
+            MovieObserver.prototype = {
+                constructor: MovieObserver,
+                update: function(){
+                    console.log("MovieObserver called");
+                }
+            };
+            function MovieObserver(){}
+            MovieObserver.prototype = {
+                constructor: MovieObserver,
+                update: function(){
+                    console.log("MovieObserver called");
+                }
+            };
+            function Movie(){
+                this.attributes = {};
+                this.handlers = [];  // <- los observers
+            };
+            Movie.prototype = {
+                constructor:Movie,
+                play:function(){
+                    console.log("Playing "+this.attributes["title"]);
+                },
+                stop:function(){
+                    console.log("stopped");
+                },
+                set:function(string, value){
+                    this.attributes[string]=value;
+                },
+                get:function(string){
+                   
+                },
+                subscribe: function(observer) {
+                    this.handlers.push(observer);
+                },
+                unsubscribe: function(observer) {
+                    this.handlers = this.handlers.filter(
+                        function(item) {
+                            if (item !== observer) {
+                                return item;
+                            }
+                        }
+                    );
+                },
+
+                notify: function() {
+                    for( i = 0; i < this.handlers.length; i++) { 
+                        handlers[i].update(); 
+                    }
+                }
+            };
+            function DownloadableMovie(){}
+            DownloadableMovie.prototype = new Movie();
+            DownloadableMovie.prototype.constructor=DownloadableMovie,
+            DownloadableMovie.prototype.download = function(){
+                   console.log("Downloaded");
+            }
+            var Social = function(){
+                 this.share = function(friendName){
+                     console.log("Sharing "+this.attributes["title"]+" with "+friendName);
+                 };
+                 this.like = function(){
+                     console.log("Liked");
+                 };
+            }
+            Social.call(Movie.prototype);
+            function Actor(actName){
+                this.name = actName;
+            }
+            Actor.prototype = {
+                constructor:Actor,
+                getName:function(){
+                    console.log("His name is "+this.name);
+                }
+            }
+            
+            
